26de283f3e0d057b042fbc43dfd492123d5e3e95
Ultrapyre <sidneymakhiutao@yahoo.com.hk>
2022-09-05 21:32:22 +0800

    Added a to-do list in the .js Good for the procrastinator!

---
+++ Assets/script.js
@@ -0,0 +1,26 @@
+//A huge-ass to-do list for later:
+/*Initialization
+Probably will make the whole thing in Javascript, partially for practice. Might make it easier to loop, who knows.
+A Title, A descriptor, and a start button.
+Has a 'High Scores' button to indicate past attempts
+    Sorted in order of score, perhaps
+*/
+
+/*The Quiz - Begins when start button pressed
+Create question, make the timer appear and tick down, and also a bunch of possible answers
+    Make a function for the timer and its functions
+    Perhaps add a spot in the code to pull question and answer sets from
+        Answers oughta be 3 wrong answers and 1 right answer... how do I randomize the options?
+        Questions shouldn't repeat, but they need to be randomized too... hmm.
+    If correct answer pressed, add a point. If wrong answer pressed, deduct time from the timer.
+        Perhaps add fancy 'point added' and 'time deducted' labels that fade away?
+Once timer hits 0, quiz ends, terminate operation
+*/
+
+/*The Evaluation - Begins when Quiz finishes
+Change website page to note the score obtained
+    Perhaps also add a 'questions answered correctly' value?
+Allows input of high score and saves it in the local directory so it persists after refresh
+    Only able to save the high score once, no need for leaderboard spam
+Whether saved high score or not, have a button to loop back to initialization stage
+ */
