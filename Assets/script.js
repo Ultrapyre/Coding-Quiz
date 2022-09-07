1a42a24447f09543c97ce177ca4bb081c8d04f64
Ultrapyre <sidneymakhiutao@yahoo.com.hk>
2022-09-07 23:18:59 +0800

    Added a landing screen and some javascripting Functionality to be added later.

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,82 @@
+//The entire website revolves around this singular root.
+//Should be possible to change things via button commands and whatnot
+var rootEl = document.getElementById('root');
+var topbarEl = document.getElementById('topbar');
+
+//Initialize some universal values, like the score and the timer
+var score = 0;
+var timeLeft = 0;
+
+//Initializes the title screen
+var landing = document.createElement("section");
+var titleEl = document.createElement("h1");
+var welcomeEl = document.createElement("p");
+titleEl.textContent = "Coding Quiz";   
+welcomeEl.textContent = "This little website will test your knowledge of Javascript!";
+landing.appendChild(titleEl);
+landing.appendChild(welcomeEl);
+
+//Initializes the quiz itself
+var timerEl = document.getElementById('timer');
+var quiz = document.createElement("section");
+var questionEl = document.createElement("div");
+
+
+
+//Initializes the Evaluation screen
+var evaluation = document.createElement("section");
+var totalScoreEl = document.createElement("p");
+
+
+//Removes anything already in the root and summons the title screen
+function titleScreen(){
+    if(rootEl.firstChild){
+        rootEl.removeChild(rootEl.firstChild);
+    }
+    rootEl.appendChild(landing);
+}
+
+
+//Removes anything in the root and starts the quiz
+function startQuiz(){
+    if(rootEl.firstChild){
+        rootEl.removeChild(rootEl.firstChild);
+    }
+    rootEl.appendChild(quiz);
+    score = 0;
+    timeLeft = 30;
+    startTimer();
+}
+
+//Starts the timer
+function startTimer(){
+    var timeInterval = setInterval(function () {
+      timerEl.textContent = timeLeft + " seconds left.";
+    if (timeLeft === 0){
+      timerEl.textContent = "";
+      clearInterval(timeInterval);
+      displayResults();
+    }
+      timeLeft--;
+  }, 1000);
+}
+
+//Generates a question and listens for right or wrong answers.
+function askQuestion(){
+
+}
+
+//Removes anything already in the root and summons the evaluation screen
+function displayResults(){
+    if(rootEl.firstChild){
+        rootEl.removeChild(rootEl.firstChild);
+    }
+    rootEl.appendChild(evaluation);
+    totalScoreEl.textContent = "Your total score is: " + score + "! You also had " + timeLeft + " seconds left on the clock!";
+}
+
+
+titleScreen();
 //A huge-ass to-do list for later:
 /*Initialization
 Probably will make the whole thing in Javascript, partially for practice. Might make it easier to loop, who knows.
